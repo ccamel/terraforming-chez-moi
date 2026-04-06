d1ae162b738f6a8786c58527c3d55a9d08563ca9
ccamel <camel.christophe@gmail.com>
2026-04-06 15:24:46 +0200

    feat(terraform): switch to prebuilt zeroclaw ghcr runtime image

--- modules/zeroclaw/main.tf
+++ modules/zeroclaw/main.tf
@@ -25,47 +25,17 @@ resource "synology_container_project" "this" {
   share_path = "${var.projects_root}/${local.project_name}"
 
   services = {
-    permfix = {
-      image = "alpine:3.22"
-      user  = "0:0"
-      name  = "${local.project_name}-permfix"
-
-      volumes = [{
-        type      = "bind"
-        source    = "./data"
-        target    = "/fix"
-        bind      = { create_host_path = true }
-        read_only = false
-      }]
-
-      entrypoint = ["sh", "-lc"]
-      command = [
-        "chown -R 65534:65534 /fix && chmod 755 /fix && echo OK"
-      ]
-
-      restart = "no"
-
-      healthcheck = {
-        test         = ["CMD-SHELL", "[ -f /bin/true ] || exit 1"]
-        interval     = "10s"
-        timeout      = "2s"
-        retries      = 1
-        start_period = "1s"
-      }
-
-      logging = { driver = "json-file" }
-    }
-
     zeroclaw = {
       image = var.image
       name  = local.project_name
 
       command = [
+        "zeroclaw",
         "daemon",
         "--host",
         "0.0.0.0",
         "--port",
-        "42617"
+        "42617",
       ]
 
       ports = [{
@@ -76,7 +46,7 @@ resource "synology_container_project" "this" {
       volumes = [{
         type      = "bind"
         source    = "./data"
-        target    = "/zeroclaw-data"
+        target    = "/root/.zeroclaw"
         bind      = { create_host_path = true }
         read_only = false
       }]
@@ -93,10 +63,6 @@ resource "synology_container_project" "this" {
 
       networks = {
         edge_net = {}
-      }
-
-      depends_on = {
-        permfix = { condition = "service_completed_successfully" }
       }
 
       logging = { driver = "json-file" }
